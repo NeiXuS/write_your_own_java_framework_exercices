7f0b3a864be209a0cdd38199542aa13a08ceb28e
NeiXuS <rdnneixus@gmail.com>
2025-09-25 18:21:33 +0200

    TP2

--- injector/src/main/java/com/github/forax/framework/injector/InjectorRegistry.java
+++ injector/src/main/java/com/github/forax/framework/injector/InjectorRegistry.java
@@ -1,5 +1,87 @@
 package com.github.forax.framework.injector;
 
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
+
 public final class InjectorRegistry {
-  // TODO
+
+    private final HashMap<Class<?>, Supplier<?>> suppliers = new HashMap<>();
+
+    public <T> void registerInstance(Class<T> type, T instance){
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(instance);
+        if(suppliers.putIfAbsent(type, () -> instance) != null)
+            throw new IllegalStateException("already in map " + type.getName());
+    }
+
+
+    public <T> T lookupInstance(Class<T> type){
+        Objects.requireNonNull(type);
+        var supplier = suppliers.get(type);
+        if(Objects.isNull(supplier)){
+            throw new IllegalStateException("key not found : " + type.getName());
+        }
+        return type.cast(supplier.get());
+    }
+
+    public <T> void registerProvider(Class<T> type, Supplier<T> supplier){
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(supplier);
+        if(suppliers.putIfAbsent(type, supplier) != null)
+            throw new IllegalStateException("already in map " + type.getName());
+    }
+
+    static List<PropertyDescriptor> findInjectableProperties(Class<?> type){
+        Objects.requireNonNull(type);
+        var beanInfos = Utils.beanInfo(type);
+        var properties = beanInfos.getPropertyDescriptors();
+        return Arrays.stream(properties)
+                .filter(property -> {
+                var setter = property.getWriteMethod();
+                return setter != null && setter.isAnnotationPresent(Inject.class);
+            })
+                .toList();
+    }
+
+    private  Constructor<?> findInjectableConstructor(Class<?> providerClass){
+        Objects.requireNonNull(providerClass);
+        var injectable = Arrays.stream(providerClass.getConstructors())
+                .filter(c -> c.isAnnotationPresent(Inject.class))
+                .toList();
+        return switch(injectable.size()){
+            case 0 -> Utils.defaultConstructor(providerClass);
+            case 1 -> injectable.getFirst();
+            default -> throw new IllegalStateException("Multiple injectable constructor" + providerClass.getName());
+        };
+    }
+
+    public <T> void registerProviderClass(Class<T> providerClass){
+        Objects.requireNonNull(providerClass);
+        registerProviderClass(providerClass, providerClass);
+    }
+
+    public <T> void registerProviderClass(Class<T> type, Class<? extends T> providerClass){
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(providerClass);
+        var constructor = findInjectableConstructor(providerClass);
+        var injectables = findInjectableProperties(type);
+        var parameterTypes = constructor.getParameterTypes();
+        registerProvider(type, () -> {
+            var args = Arrays.stream(parameterTypes)
+                    .map(this::lookupInstance)
+                    .toArray();
+            var object = Utils.newInstance(constructor, args);
+            for(var injectable : injectables){
+                var setter = injectable.getWriteMethod();
+                var value = lookupInstance(injectable.getPropertyType());
+                Utils.invokeMethod(object, setter, value);
+            }
+            return type.cast(object);
+        });
+    }
 }
--- interceptor/src/main/java/org/github/forax/framework/interceptor/InterceptorRegistry.java
+++ interceptor/src/main/java/org/github/forax/framework/interceptor/InterceptorRegistry.java
@@ -3,15 +3,108 @@ package org.github.forax.framework.interceptor;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
+import java.sql.Array;
+import java.util.*;
 import java.util.stream.Stream;
 
 public final class InterceptorRegistry {
-  private AroundAdvice advice;
+  private final Map<Class<? extends Annotation>, List<AroundAdvice>> adviceList = new HashMap<>();
+  private final Map<Class<? extends Annotation>, List<Interceptor>> interceptorsList = new HashMap<>();
 
-  // TODO
+//  public void addAroundAdvice(Class<? extends Annotation> annotationClass, AroundAdvice aroundAdvice){
+//    Objects.requireNonNull(annotationClass);
+//    Objects.requireNonNull(aroundAdvice);
+//    adviceList.computeIfAbsent(annotationClass, _ -> new ArrayList<>()).add(aroundAdvice);
+//  }
+
+  public void addAroundAdvice(Class<? extends Annotation> annotationClass, AroundAdvice aroundAdvice){
+      Objects.requireNonNull(annotationClass);
+      Objects.requireNonNull(aroundAdvice);
+      addInterceptor(annotationClass, (o, m, a, i) -> {
+        aroundAdvice.before(o, m, a);
+        Object result = null;
+        try{
+          result = i.proceed(o,m,a);
+        }finally {
+          aroundAdvice.after(o,m,a, result);
+        }
+        return result;
+      });
+    }
+/*
+  List<AroundAdvice> findAdvices(Method method){
+    var annotations = method.getAnnotations();
+    return Arrays.stream(annotations)
+            .flatMap(annotation -> {
+              var annType = annotation.annotationType();
+              var advices = adviceList.getOrDefault(annType, List.of());
+              return advices.stream();
+            })
+            .toList();
+  }*/
+
+//  public <T> T createProxy(Class<T> type, T implementation){
+//    Objects.requireNonNull(type);
+//    Objects.requireNonNull(implementation);
+//    if(!type.isInterface()){
+//      throw new IllegalArgumentException("type isnt interface : " + type.getName());
+//    }
+//    return type.cast(Proxy.newProxyInstance(type.getClassLoader(),
+//        new Class<?>[] {type},
+//        (_, method, args) -> {
+//          var advices = findAdvices(method);
+//          for(var advice : advices){
+//            advice.before(implementation, method, args);
+//          }
+//          Object result = null;
+//          try{
+//            result  = Utils.invokeMethod(implementation, method, args);
+//          }finally{
+//            for(var advice : advices.reversed()){
+//              advice.after(implementation, method, args, result);
+//            }
+//          }
+//          return result;
+//        }));
+//  }
+
+  public <T> T createProxy(Class<T> type, T implementation){
+    Objects.requireNonNull(type);
+    Objects.requireNonNull(implementation);
+    if(!type.isInterface()){
+      throw new IllegalArgumentException("type isnt interface : " + type.getName());
+    }
+    return type.cast(Proxy.newProxyInstance(type.getClassLoader(),
+            new Class<?>[] {type},
+            (_, method, args) -> {
+              var invocation = getInvocation(findInterceptors(method));
+              return invocation.proceed(implementation, method, args);
+            }));
+  }
+
+  public void addInterceptor(Class<? extends Annotation> annotationClass, Interceptor interceptor){
+    Objects.requireNonNull(annotationClass);
+    Objects.requireNonNull(interceptor);
+    interceptorsList.computeIfAbsent(annotationClass, _ -> new ArrayList<>()).add(interceptor);
+  }
+
+  static Invocation getInvocation(List<Interceptor> interceptors){
+    Invocation invocation = Utils::invokeMethod;
+    for(var interceptor : interceptors.reversed()){
+      var copyInvocation = invocation;
+      invocation = (o, m, a) -> interceptor.intercept(o,m,a, copyInvocation);
+    }
+    return invocation;
+  }
+
+  List<Interceptor> findInterceptors(Method method){
+    var annotations = method.getAnnotations();
+    return Arrays.stream(annotations)
+            .flatMap(annotation -> {
+              var annType = annotation.annotationType();
+              var advices = interceptorsList.getOrDefault(annType, List.of());
+              return advices.stream();
+            })
+            .toList();
+  }
 }
